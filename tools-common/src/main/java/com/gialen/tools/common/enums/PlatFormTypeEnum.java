2e4d82fd3f46661d2c21369137a485f394276635
wong <1>
2019-09-27 18:01:52 +0800

    对接新系统数据库的统计

--- tools-common/src/main/java/com/gialen/tools/common/enums/PlatFormTypeEnum.java
+++ tools-common/src/main/java/com/gialen/tools/common/enums/PlatFormTypeEnum.java
@@ -3,8 +3,8 @@ package com.gialen.tools.common.enums;
 public enum PlatFormTypeEnum {
 
     APP("1", "app"),
-    H5("2", "手机H5"),
-    WX_MINI_PROGRAM("3", "小程序");
+    WX_MINI_PROGRAM("2", "小程序"),
+    H5("3", "手机H5");
 //    GROUP_WX_PUBLIC_ACCOUNTS("GROP_WX_PUBLIC_ACCOUNTS", "集团公众号"),
 
     private String code;
