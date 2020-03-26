2577181f79f39958e45562256b79cfa17cc5a7ec
wong <1>
2020-03-26 15:05:24 +0800

    掌数宝店董新增通联帐户余额查询⬇x3

--- tools-api/src/main/java/com/gialen/tools/api/ToolsApiApplication.java
+++ tools-api/src/main/java/com/gialen/tools/api/ToolsApiApplication.java
@@ -1,5 +1,7 @@
 package com.gialen.tools.api;
 
+import com.gialen.common.annotation.GialenApp;
+import com.gialen.rpc.dubbo.GialenDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.gialen.tools"})
+@GialenDubbo(scanBasePackages = {"com.gialen.tools"})
 public class ToolsApiApplication {
 
     public static void main(String[] args) {
